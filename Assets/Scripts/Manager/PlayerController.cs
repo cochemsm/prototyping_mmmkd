90201be6b43622cbd400040b65a66a185ce5f140
Magnus Cochems <magnus@cochems.net>
2024-06-03 19:02:36 +0200

    player controller

--- Assets/Scripts/Manager/PlayerController.cs
+++ Assets/Scripts/Manager/PlayerController.cs
@@ -8,12 +8,16 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
     [SerializeField] private InputActionReference playerinput;
     [SerializeField] private float movementSpeed = 3f;
-    [SerializeField] private float lookSpeed = 3f;
+    [SerializeField] private float lookSpeed = 0.5f;
     private Vector2 input;
+    Vector3 moveDirection = Vector3.zero;
+    private float rotationX;
 
     private void Awake() {
         rb = GetComponent<Rigidbody>();
         rb.velocity = Vector3.zero;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void OnEnable() {
@@ -22,8 +26,14 @@ public class PlayerController : MonoBehaviour {
 
     private void Update() {
         input = playerinput.action.ReadValue<Vector2>();
-        Vector2 mouseMovement = Mouse.current.delta.ReadValue();
-        transform.GetChild(0).rotation = Quaternion.Euler(transform.GetChild(0).rotation.x + mouseMovement.x, transform.GetChild(0).rotation.y + mouseMovement.y, 0);
+        if (input == Vector2.zero) rb.velocity = Vector2.zero;
+        rb.MovePosition(transform.position + (transform.forward * input.y * movementSpeed) + (transform.right * input.x * movementSpeed));
+
+        Vector2 mouseMovement = Mouse.current.delta.ReadValue() * lookSpeed;
+        rotationX += -mouseMovement.y;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+        transform.GetChild(0).transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, mouseMovement.x, 0);
     }
 
     private void FixedUpdate() {
