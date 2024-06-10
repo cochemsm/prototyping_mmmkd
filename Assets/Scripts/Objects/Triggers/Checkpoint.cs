8c0266f464289e94bca4dae0200e8beea1f37ea4
Magnus Cochems <magnus@cochems.net>
2024-06-10 18:11:47 +0200

    change checkpoint code

--- Assets/Scripts/Objects/Triggers/Checkpoint.cs
+++ Assets/Scripts/Objects/Triggers/Checkpoint.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 namespace Objects.Triggers {
     [RequireComponent(typeof(BoxCollider))]
     public class Checkpoint : MonoBehaviour {
+        private bool canSafe = true;
+        
         private void Awake() {
             GetComponent<BoxCollider>().isTrigger = true;
         }
 
         private void OnTriggerEnter(Collider other) {
+            if (!canSafe) return;
             if (!other.CompareTag("Player")) return;
             PublicEvents.SafeGame?.Invoke();
-            Destroy(gameObject);
+            canSafe = false;
         }
     }
 }
