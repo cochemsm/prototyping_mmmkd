1da4353d197da5eba63b77945b4b15ba7cae3fd7
Magnus Cochems <magnus@cochems.net>
2024-06-13 13:14:54 +0200

    look rotation

---
+++ Assets/Scripts/Objects/Map/LookTowards.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Objects.Map {
+    public class LookToward : MonoBehaviour {
+        [SerializeField] private GameObject target;
+
+        private void Awake() {
+            transform.rotation = Quaternion.LookRotation(target.transform.position);
+        }
+    }
+}
