2302a417f0effe2c0c49832720ae78a734ab9643
Magnus Cochems <magnus@cochems.net>
2024-06-15 14:45:12 +0200

    removed main camera

--- Assets/Scripts/Objects/Interactables/Lever.cs
+++ Assets/Scripts/Objects/Interactables/Lever.cs
@@ -15,11 +15,13 @@ namespace Objects.Interactables {
 
         public void Interact() {
             // Lever animation trigger
+            Debug.Log("Lever Turned");
             _trigger.enabled = false;
             GameManager.Instance.PreventTriggerBug();
             _leversTurned++;
             if (_leversTurned >= 3) {
                 PublicEvents.PuzzleIsRight?.Invoke();
+                Debug.Log("All Levers Turned");
             }
         }
     }
