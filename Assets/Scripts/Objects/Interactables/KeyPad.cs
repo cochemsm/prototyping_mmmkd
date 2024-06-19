667570d402d0062f728079d2110293a6db346f95
Magnus Cochems <magnus@cochems.net>
2024-06-19 23:12:10 +0200

    keypad fix

--- Assets/Scripts/Objects/Interactables/KeyPad.cs
+++ Assets/Scripts/Objects/Interactables/KeyPad.cs
@@ -3,12 +3,7 @@ using Manager;
 using UnityEngine;
 
 namespace Objects.Interactables {
-    [RequireComponent(typeof(SphereCollider))]
     public class KeyPad : MonoBehaviour, IInteractable {
-        private void Awake() {
-            GetComponent<SphereCollider>().isTrigger = true; 
-        }
-
         public void Interact() {
             UIController.Instance.ChangePanel(UIController.UIs.KeyPad);
             PublicEvents.LockPlayerMovementToggle?.Invoke();
